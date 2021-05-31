10b9387c6f7958a5ed1c0097b4253691a0ba48b3
Emizzari <emilie@tonnessen.com>
2021-05-31 11:35:24 +0200

    Adding form validation with react-hook-form and yup

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,33 +1,81 @@
 import { Form, Button, FormControl, InputGroup } from 'react-bootstrap';
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object().shape({
+    name: yup.string().required("Please enter your name"),
+    email: yup.string().required("Please enter an email address").email("Please enter a valid email address"),
+    website: yup.string().required("Please enter your website"),
+    message: yup.string().required("Please enter your message").min(10, "The message must be at least 10 characters"),
+});
 
 const ContactForm: React.FC = () => {
+
+    // react-hook-form + yup resolver
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema),
+    });
+
+
+
+    // Submitting Data
+    const onSubmit = (data: any) => {
+        console.log("[data]", data);
+    }
+
+
+    // console logs
+    console.error("[errors]", errors);
+
     return (
-        <Form className="form">
-            <Form.Group className="form__name">
-                <Form.Label className="form__label">Name</Form.Label>
-                <Form.Control placeholder="First name" />
+        <Form className="form" onSubmit={handleSubmit(onSubmit)}>
+
+            {/* Name */}
+            <Form.Group>
+                <Form.Label>Name</Form.Label>
+                <Form.Control placeholder="First name" {...register("name")} />
+                {errors.name && <span className="form__group--error">{errors.name.message}</span>}
             </Form.Group>
-            <Form.Group className="form__mail" controlId="exampleForm.ControlInput1">
-                <Form.Label className="form__label">Email</Form.Label>
-                <Form.Control type="email" placeholder="name@example.com" />
+
+
+            {/* Email */}
+            <Form.Group  controlId="exampleForm.ControlInput1">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="name@example.com" {...register("email")}/>
+                {errors.email && <span className="form__group--error">{errors.email.message}</span>}
             </Form.Group>
-            <Form.Group className="form__website">
-                <Form.Label className="form__label">Website</Form.Label>
+
+
+            {/* Website */}
+            <Form.Group>
+                <Form.Label>Website</Form.Label>
                 <InputGroup className="mb-2">
                     <InputGroup.Prepend>
                         <InputGroup.Text>https://</InputGroup.Text>
                     </InputGroup.Prepend>
-                    <FormControl id="inlineFormInputGroup" placeholder="www.example.com" />
+                    <FormControl id="inlineFormInputGroup" placeholder="www.example.com" {...register("website")}/>
                 </InputGroup>
+                {errors.website && <span className="form__group--error">{errors.website.message}</span>}
             </Form.Group>
-            <Form.Group className="form__msg" controlId="exampleForm.ControlTextarea1">
-                <Form.Label className="form__label">Message</Form.Label>
-                <Form.Control as="textarea" rows={3} />
+
+
+            {/* Message */}
+            <Form.Group  controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Message</Form.Label>
+                <Form.Control as="textarea" rows={3} {...register("message")}/>
+                {errors.message && <span className="form__group--error">{errors.message.message}</span>}
             </Form.Group>
-            <Form.Group className="form__checkbox" controlId="formBasicCheckbox">
-                <Form.Check label="Allow us to sell your personal details to whomever we want" />
+
+
+            {/* Checkbox */}
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Allow us to sell your personal details to whomever we want" />
             </Form.Group>
-            <Button className="form__btn" variant="primary" type="submit">
+
+
+            {/* Submit Button */}
+            <Button variant="primary" type="submit">
                 Submit
             </Button>
         </Form>
